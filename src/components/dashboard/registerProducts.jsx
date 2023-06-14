3a44ffbd5998eb11035f86b90de3ecc6b8da2c31
mateusdata <mateuspele2015@gmail.com>
2023-06-14 04:23:48 -0300

    Add: new routes

--- src/components/dashboard/registerProducts.jsx
+++ src/components/dashboard/registerProducts.jsx
@@ -222,7 +222,7 @@ export default function RegisterProducts() {
           disabled={inputDisable}
           onClick={cadastrarAlimento}
           type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 
         >
           Salvar
--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -57,7 +57,7 @@ export default function LoginPage() {
                     type="text"
                     autoComplete="email"
                     required
-                    className="block w-full  bg-gray-200 text-2xl rounded-md border-0 py-1.5 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full  bg-gray-950 text-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -80,7 +80,7 @@ export default function LoginPage() {
                     type="password"
                     autoComplete="current-password"
                     required
-                    className="block w-full  bg-gray-200 text-2xl rounded-md border-0 py-1.5 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full  bg-gray-950 text-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
