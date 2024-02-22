1e1bf53547d2a77e319f9ae780684b73ff923dbe
Mateus Santos <54743403+mateusdata@users.noreply.github.com>
2024-02-22 09:21:06 -0300

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,13 @@
 
 import axios from 'axios';
 
+// Importe a variável de ambiente BACKEND_PORT
+const BACKEND_PORT = process.env.BACKEND_PORT;
+
+// Verifique se a variável BACKEND_PORT está definida
+
+
 // Set the global base URL for Axios
-axios.defaults.baseURL = 'http://backend:3002';
+axios.defaults.baseURL = `http://localhost:${BACKEND_PORT || 3002}`;
 
 export default axios;
-    
